548e61fa9571519ee29ecb8aea87f711c63f87f2
Ashton Bradley <ashton.bradley@gmail.com>
2018-01-05 13:23:34 +1300

    rename plaguerre

--- src/PhaseSpaceTools.jl
+++ src/PhaseSpaceTools.jl
@@ -4,6 +4,7 @@ module PhaseSpaceTools
 
 using PyCall, SymPy, Distributions
 
+#import laguerre from scipy.special
 const scipy_spec = PyNULL()
 
 function __init__()
@@ -15,13 +16,13 @@ end
 
 include("reject.jl")
 include("laguerre.jl")
-include("PlaguerreN.jl")
+include("plaguerre.jl")
 include("coherent.jl")
 include("thermal.jl")
 include("squeezed.jl")
 include("fock.jl")
 include("crescent.jl")
 
-export coherent, thermal, squeezed, fock, crescent, reject, laguerre, PlaguerreN
+export coherent, thermal, squeezed, fock, crescent
 
 end #end of module
--- src/plaguerre.jl
+++ src/plaguerre.jl
@@ -1,4 +1,4 @@
-function PlaguerreN(x,n)
+function plaguerre(x,n)
     #helper function for +W Fock state
 #P(x,n)=(4/3)*exp(-2*x^2/3)*x*laguerre(-4*x^2/3,n)/(3^n)
     return (4/3)*exp(-2*x^2/3 + log(x) + log(laguerre(-4*x^2/3,n)) - n*log(3))
