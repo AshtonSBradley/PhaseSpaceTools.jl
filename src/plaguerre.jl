06b5d0b7cf3f24eb482c00bf5bb6f984b6d27992
AshtonSBradley <ashton.bradley@otago.ac.nz>
2018-01-09 09:33:50 +1300

    update docstrings for new functions

--- src/plaguerre.jl
+++ src/plaguerre.jl
@@ -1,3 +1,15 @@
+"""
+```julia
+P(x,n) = plaguerre(x,n)
+```
+Radial phase space probability distribution for fock state sampling in
++P representation.
+
+`x ` is location to evaluate probability.
+
+`n` is the number of the fock state.
+
+"""
 function plaguerre(x,n)
     #helper function for +W Fock state
 #P(x,n)=(4/3)*exp(-2*x^2/3)*x*laguerre(-4*x^2/3,n)/(3^n)
--- src/plaguerre_asymptotic.jl
+++ src/plaguerre_asymptotic.jl
@@ -1,3 +1,15 @@
+"""
+```julia
+P(x,n) = plaguerre_asymptotic(x,n)
+```
+Radial phase space probability distribution for fock state sampling in
++P representation, in the large `n` limit - asymptotic expansion.
+
+`x ` is location to evaluate probability.
+
+`n` is the number of the fock state.
+
+"""
 function plaguerre_asymptotic(x,n)
 return exp(-(x-sqrt(n+1))^2)/sqrt(pi)
 end
