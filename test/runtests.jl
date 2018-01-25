a598f3b0a12f80171c01cc4dcd7d68f74431f3c7
Ashton Bradley <ashton.bradley@gmail.com>
2018-01-25 19:01:54 +1300

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,9 +10,19 @@ include("samplefockW.jl")
 @testset "fock state W tests " begin include("testfockW.jl") end
 
 include("samplefock+W.jl")
-@testset "fock state W tests " begin include("testfock+W.jl") end
+@testset "fock state +W tests " begin include("testfock+W.jl") end
 
 include("samplefock+Wasymp.jl")
-@testset "fock state W tests " begin include("testfock+Wasymp.jl") end
+@testset "fock state +W tests for large n " begin include("testfock+Wasymp.jl") end
 
 #test squeezed
+include("samplesqueezed+P.jl")
+@testset "squeezed state +P tests " begin include("testsqueezed+P.jl") end
+
+#test squeezed
+include("samplesqueezedW.jl")
+@testset "squeezed state W tests " begin include("testsqueezed+P.jl") end
+
+
+#test thermal
+#include("samplethermalP.jl")
---
+++ test/testsqueezed+P.jl
@@ -0,0 +1,2 @@
+@test norm(av_a - β)/abs(β) < 0.01
+@test abs(n̄ - nbar)/abs(β)^2 < 0.01
